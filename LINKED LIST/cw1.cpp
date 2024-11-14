e609b9a71c0475eaa6d9d97a11cfad607a565e89
Ajaybalaji2115 <150769063+Ajaybalaji2115@users.noreply.github.com>
2024-11-14 11:55:31 +0530

    Create cw1.cpp

---
+++ LINKED LIST/cw1.cpp
@@ -0,0 +1,18 @@
+// //Write a program to generate nth Fibonacci term using recursion.
+// //Input 1 :
+// 10
+// Output 1 :
+// 55
+//   Input 2 :
+// 20
+// Output 2 :
+// 6765
+//   Input 3 :
+// 1
+// Output 3 :
+// 1
+//   //Input 4 :
+// 5
+// Output 4 :
+// 5
+
